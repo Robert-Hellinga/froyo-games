e0afa485f77341f606c2fcf414d02cf924e4ee5e
Austenoooo <yixuan.li683@dukekunshan.edu.cn>
2020-10-29 15:16:40 +0800

    update GridGame

--- src/ooga/game/GridGame.java
+++ src/ooga/game/GridGame.java
@@ -1,9 +1,29 @@
 package ooga.game;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 public class GridGame {
   public enum BlockState {
     CHECKER_PLAYER_1,
     CHECKER_PLAYER_2,
     EMPTY,
+  }
+  public final static int SIZE = 8;
+
+  private BlockStructure allBlocks;
+  private String gameType;
+
+  public GridGame(String gameType){
+    this.gameType = gameType;
+  }
+
+  private void createAllBlock(){
+    Collection<Collection<Block>> initialAllBlocks = new ArrayList<>();
+    for (int i = 0; i < SIZE; i++){
+      for (int j = 0; j < SIZE; j++){
+
+      }
+    }
   }
 }
