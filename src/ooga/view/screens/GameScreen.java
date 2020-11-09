133cfa4437617caa0d5166f39c863047655a200e
Nate Mela <nrm27@duke.edu>
2020-11-09 14:50:35 -0500

    Updated alignment for GameScreen gridpane, added PlayerTurnBox element

--- src/ooga/view/screens/GameScreen.java
+++ src/ooga/view/screens/GameScreen.java
@@ -1,15 +1,18 @@
 package ooga.view.screens;
 
 import java.util.Locale;
+import javafx.geometry.Pos;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 import ooga.controller.IGameController;
 import ooga.fileHandler.Resources;
 import ooga.model.game.Game;
+import ooga.model.player.Player;
 import ooga.view.GameObserver;
 import ooga.view.Styleable;
 import ooga.view.Util;
 import ooga.view.elements.GameScreenButtonBox;
+import ooga.view.elements.PlayerTurnBox;
 import ooga.view.grid.PieceGrid;
 
 public class GameScreen extends GridPane implements Styleable, GameObserver {
@@ -18,7 +21,7 @@ public class GameScreen extends GridPane implements Styleable, GameObserver {
   private static final String RESOURCE_FILE = "GameScreen";
   private static final int SCREEN_WIDTH = 400;
   private static final int SCREEN_HEIGHT = 350;
-  private static final int SCREEN_H_SPACING = 10;
+  private static final int SCREEN_H_SPACING = 20;
   private static final int SCREEN_V_SPACING = 10;
 
   private IGameController controller;
@@ -35,14 +38,19 @@ public class GameScreen extends GridPane implements Styleable, GameObserver {
     resources = new Resources(this.locale, Resources.UI_RESOURCE_PACKAGE, RESOURCE_FILE);
     grid = new PieceGrid(controller, game.getAllBlockStates());
 
+    setAlignment(Pos.CENTER);
     setStyleSheet(DEFAULT_STYLE_SHEET);
     setWidth(SCREEN_WIDTH);
     setHeight(SCREEN_HEIGHT);
     setVgap(SCREEN_V_SPACING);
     setHgap(SCREEN_H_SPACING);
-    
-    add(new GameScreenButtonBox(resources, controller), 0, 0);
-    add(grid, 1, 0);
+
+    setGridLinesVisible(true);
+
+    add(new PlayerTurnBox(), 1, 0);
+    add(new GameScreenButtonBox(resources, controller), 0, 1);
+    add(grid, 1, 1);
+    add(new PlayerTurnBox(), 1, 2);
   }
 
   @Override
