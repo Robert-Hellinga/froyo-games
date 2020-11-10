c6e8058ba2493baf7905956a7991d7294c7a8865
Lucas Carter <lbc21@duke.edu>
2020-11-09 20:01:32 -0500

    Pushing to pull from master (starting tests righ now)

--- test/ooga/controller/FroyoControllerTest.java
+++ test/ooga/controller/FroyoControllerTest.java
@@ -2,13 +2,28 @@ package ooga.controller;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+import org.junit.jupiter.api.Test;
 import util.DukeApplicationTest;
 
 class FroyoControllerTest extends DukeApplicationTest {
 
+  private IFroyoController froyoController;
+
+
   @Override
   public void start(Stage stage){
+    stage.show();
+    froyoController = new FroyoController(stage);
+
+  }
+
+  @Test
+  public void testSwitchingLayout(){
+    Pane testPane = new Pane();
+    testPane.setId("testPane");
+    froyoController.setNewLayout(testPane);
 
   }
 
