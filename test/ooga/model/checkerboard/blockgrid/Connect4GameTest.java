8755ebf1815913ee25ee8a9905c790cde4366c9e
Austenoooo <yixuan.li683@dukekunshan.edu.cn>
2020-11-10 16:00:10 +0800

    added test for checkers model

---
+++ test/ooga/model/checkerboard/blockgrid/Connect4GameTest.java
@@ -0,0 +1,47 @@
+package ooga.model.checkerboard.blockgrid;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import ooga.Coordinate;
+import ooga.controller.GameController.PlayerMode;
+import ooga.model.game.Connect4Game;
+import ooga.model.game.Game;
+import org.junit.jupiter.api.Test;
+
+class Connect4GameTest {
+
+  private final String gameType = "Connect4";
+  private final String playerName = "a";
+  private final PlayerMode playerMode = PlayerMode.PLAY_WITH_FRIEND;
+  private final String startPattern = "test";
+  private Game connect4Game;
+
+  @Test
+  public void testDropPiece(){
+    connect4Game = new Connect4Game(gameType, playerName, playerMode, startPattern);
+    Coordinate pieceToPress = new Coordinate(0,0);
+    connect4Game.play(pieceToPress);
+    List<List<Integer>> expectedBlockState = new ArrayList<>(){{
+      add(List.of(0,0,0,0));
+      add(List.of(0,0,0,0));
+      add(List.of(1,0,0,0));
+    }};
+    assertEquals(expectedBlockState, connect4Game.getAllBlockStates());
+  }
+
+  @Test
+  public void testDropPieceOnOtherPiece(){
+    connect4Game = new Connect4Game(gameType, playerName, playerMode, startPattern);
+    Coordinate pieceToPress = new Coordinate(0,0);
+    connect4Game.play(pieceToPress);
+    Coordinate pieceToPress2 = new Coordinate(0,0);
+    connect4Game.play(pieceToPress2);
+    List<List<Integer>> expectedBlockState = new ArrayList<>(){{
+      add(List.of(0,0,0,0));
+      add(List.of(2,0,0,0));
+      add(List.of(1,0,0,0));
+    }};
+    assertEquals(expectedBlockState, connect4Game.getAllBlockStates());
+  }
+}
