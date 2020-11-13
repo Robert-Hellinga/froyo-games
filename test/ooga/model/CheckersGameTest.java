3b1ce3c8475d651635d46604c9fdb0c977856da9
Lucas Carter <lbc21@duke.edu>
2020-11-12 20:40:00 -0500

    Adjusted tests to work with refactored Game constructor (takes in player objects). Closes #9

--- test/ooga/model/CheckersGameTest.java
+++ test/ooga/model/CheckersGameTest.java
@@ -7,6 +7,8 @@ import ooga.Coordinate;
 import ooga.controller.FroyoController;
 import ooga.controller.GameController.PlayerMode;
 import ooga.model.game.Game;
+import ooga.model.player.HumanPlayer;
+import ooga.model.player.Player;
 import org.junit.jupiter.api.Test;
 
 class CheckersGameTest{
@@ -17,71 +19,81 @@ class CheckersGameTest{
   private final String startPattern = "test";
   private Game checkersGame;
 
-//  @Test
-//  public void testChooseAPiece(){
-//    checkersGame = FroyoController.createGame(gameType, playerName, playerMode, startPattern);
-//    Coordinate pieceToChoose = new Coordinate(0,0);
-//    checkersGame.play(pieceToChoose);
-//    assertTrue(checkersGame.getBoard().getAllBlocks().getBlock(pieceToChoose).getBlockState().isChosen());
-//  }
-//
-//  @Test
-//  public void testChooseAPieceAndMoveIt(){
-//    checkersGame = FroyoController.createGame(gameType, playerName, playerMode, startPattern);
-//    Coordinate pieceToChoose = new Coordinate(2,0);
-//    Coordinate pieceToMoveTo = new Coordinate(3,1);
-//    checkersGame.play(pieceToChoose);
-//    checkersGame.play(pieceToMoveTo);
-//    List<List<Integer>> expectedBlockState = new ArrayList<>(){{
-//      add(List.of(1,0,0,0));
-//      add(List.of(0,2,0,1));
-//      add(List.of(0,0,0,0));
-//    }};
-//    assertEquals(expectedBlockState, checkersGame.getAllBlockStates());
-//  }
-//
-//  @Test
-//  public void testChooseAPieceAndCrossAnotherPiece(){
-//    checkersGame = FroyoController.createGame(gameType, playerName, playerMode, startPattern);
-//    Coordinate pieceToChoose = new Coordinate(0,0);
-//    Coordinate pieceToMoveTo = new Coordinate(2,2);
-//    checkersGame.play(pieceToChoose);
-//    checkersGame.play(pieceToMoveTo);
-//    List<List<Integer>> expectedBlockState = new ArrayList<>(){{
-//      add(List.of(0,0,1,0));
-//      add(List.of(0,0,0,0));
-//      add(List.of(0,0,6,0));
-//    }};
-//    assertEquals(expectedBlockState, checkersGame.getAllBlockStates());
-//  }
-//
-//  @Test
-//  public void testAPieceBecomeKingWhenItReachesOtherEnd(){
-//    checkersGame = FroyoController.createGame(gameType, playerName, playerMode, startPattern);
-//    Coordinate pieceToChoose = new Coordinate(0,0);
-//    Coordinate pieceToMoveTo = new Coordinate(2,2);
-//    checkersGame.play(pieceToChoose);
-//    checkersGame.play(pieceToMoveTo);
-//    assertTrue(checkersGame.getBoard().getAllBlocks().getBlock(pieceToMoveTo).getBlockState().isKing());
-//  }
-//
-//  @Test
-//  public void testIfKingPieceCanMoveBackward(){
-//    checkersGame = FroyoController.createGame(gameType, playerName, playerMode, startPattern);
-//    Coordinate pieceToChoose = new Coordinate(0,0);
-//    Coordinate pieceToKing = new Coordinate(2,2);
-//    Coordinate pieceKingMoveTo = new Coordinate(1,1);
-//    checkersGame.play(pieceToChoose);
-//    checkersGame.play(pieceToKing);
-//    checkersGame.playerTakeTurn();
-//    checkersGame.play(pieceToKing);
-//    checkersGame.play(pieceKingMoveTo);
-//    List<List<Integer>> expectedBlockState = new ArrayList<>(){{
-//      add(List.of(0,0,1,0));
-//      add(List.of(0,6,0,0));
-//      add(List.of(0,0,0,0));
-//    }};
-//    assertEquals(expectedBlockState, checkersGame.getAllBlockStates());
-//  }
+  @Test
+  public void testChooseAPiece(){
+    Player playerOne = new HumanPlayer("Player 1");
+    Player playerTwo = new HumanPlayer("Player 2");
+    checkersGame = FroyoController.createGame(gameType, playerOne, playerTwo, startPattern);
+    Coordinate pieceToChoose = new Coordinate(0,0);
+    checkersGame.play(pieceToChoose);
+    assertTrue(checkersGame.getBoard().getAllBlocks().getBlock(pieceToChoose).getBlockState().isChosen());
+  }
+
+  @Test
+  public void testChooseAPieceAndMoveIt(){
+    Player playerOne = new HumanPlayer("Player 1");
+    Player playerTwo = new HumanPlayer("Player 2");
+    checkersGame = FroyoController.createGame(gameType, playerOne, playerTwo, startPattern);
+    Coordinate pieceToChoose = new Coordinate(2,0);
+    Coordinate pieceToMoveTo = new Coordinate(3,1);
+    checkersGame.play(pieceToChoose);
+    checkersGame.play(pieceToMoveTo);
+    List<List<Integer>> expectedBlockState = new ArrayList<>(){{
+      add(List.of(1,0,0,0));
+      add(List.of(0,2,0,1));
+      add(List.of(0,0,0,0));
+    }};
+    assertEquals(expectedBlockState, checkersGame.getAllBlockStates());
+  }
+
+  @Test
+  public void testChooseAPieceAndCrossAnotherPiece(){
+    Player playerOne = new HumanPlayer("Player 1");
+    Player playerTwo = new HumanPlayer("Player 2");
+    checkersGame = FroyoController.createGame(gameType, playerOne, playerTwo, startPattern);
+    Coordinate pieceToChoose = new Coordinate(0,0);
+    Coordinate pieceToMoveTo = new Coordinate(2,2);
+    checkersGame.play(pieceToChoose);
+    checkersGame.play(pieceToMoveTo);
+    List<List<Integer>> expectedBlockState = new ArrayList<>(){{
+      add(List.of(0,0,1,0));
+      add(List.of(0,0,0,0));
+      add(List.of(0,0,6,0));
+    }};
+    assertEquals(expectedBlockState, checkersGame.getAllBlockStates());
+  }
+
+  @Test
+  public void testAPieceBecomeKingWhenItReachesOtherEnd(){
+    Player playerOne = new HumanPlayer("Player 1");
+    Player playerTwo = new HumanPlayer("Player 2");
+    checkersGame = FroyoController.createGame(gameType, playerOne, playerTwo, startPattern);
+    Coordinate pieceToChoose = new Coordinate(0,0);
+    Coordinate pieceToMoveTo = new Coordinate(2,2);
+    checkersGame.play(pieceToChoose);
+    checkersGame.play(pieceToMoveTo);
+    assertTrue(checkersGame.getBoard().getAllBlocks().getBlock(pieceToMoveTo).getBlockState().isKing());
+  }
+
+  @Test
+  public void testIfKingPieceCanMoveBackward(){
+    Player playerOne = new HumanPlayer("Player 1");
+    Player playerTwo = new HumanPlayer("Player 2");
+    checkersGame = FroyoController.createGame(gameType, playerOne, playerTwo, startPattern);
+    Coordinate pieceToChoose = new Coordinate(0,0);
+    Coordinate pieceToKing = new Coordinate(2,2);
+    Coordinate pieceKingMoveTo = new Coordinate(1,1);
+    checkersGame.play(pieceToChoose);
+    checkersGame.play(pieceToKing);
+    checkersGame.playerTakeTurn();
+    checkersGame.play(pieceToKing);
+    checkersGame.play(pieceKingMoveTo);
+    List<List<Integer>> expectedBlockState = new ArrayList<>(){{
+      add(List.of(0,0,1,0));
+      add(List.of(0,6,0,0));
+      add(List.of(0,0,0,0));
+    }};
+    assertEquals(expectedBlockState, checkersGame.getAllBlockStates());
+  }
 
 }
--- test/ooga/model/Connect4GameTest.java
+++ test/ooga/model/Connect4GameTest.java
@@ -7,6 +7,8 @@ import ooga.Coordinate;
 import ooga.controller.GameController.PlayerMode;
 import ooga.model.game.Connect4Game;
 import ooga.model.game.Game;
+import ooga.model.player.HumanPlayer;
+import ooga.model.player.Player;
 import org.junit.jupiter.api.Test;
 
 class Connect4GameTest {
@@ -17,31 +19,35 @@ class Connect4GameTest {
   private final String startPattern = "test";
   private Game connect4Game;
 
-//  @Test
-//  public void testDropPiece(){
-//    connect4Game = new Connect4Game(gameType, playerName, playerMode, startPattern);
-//    Coordinate pieceToPress = new Coordinate(0,0);
-//    connect4Game.play(pieceToPress);
-//    List<List<Integer>> expectedBlockState = new ArrayList<>(){{
-//      add(List.of(0,0,0,0));
-//      add(List.of(0,0,0,0));
-//      add(List.of(1,0,0,0));
-//    }};
-//    assertEquals(expectedBlockState, connect4Game.getAllBlockStates());
-//  }
-//
-//  @Test
-//  public void testDropPieceOnOtherPiece(){
-//    connect4Game = new Connect4Game(gameType, playerName, playerMode, startPattern);
-//    Coordinate pieceToPress = new Coordinate(0,0);
-//    connect4Game.play(pieceToPress);
-//    Coordinate pieceToPress2 = new Coordinate(0,0);
-//    connect4Game.play(pieceToPress2);
-//    List<List<Integer>> expectedBlockState = new ArrayList<>(){{
-//      add(List.of(0,0,0,0));
-//      add(List.of(2,0,0,0));
-//      add(List.of(1,0,0,0));
-//    }};
-//    assertEquals(expectedBlockState, connect4Game.getAllBlockStates());
-//  }
+  @Test
+  public void testDropPiece(){
+    Player playerOne = new HumanPlayer("Player 1");
+    Player playerTwo = new HumanPlayer("Player 2");
+    connect4Game = new Connect4Game(gameType, playerOne, playerTwo, startPattern);
+    Coordinate pieceToPress = new Coordinate(0,0);
+    connect4Game.play(pieceToPress);
+    List<List<Integer>> expectedBlockState = new ArrayList<>(){{
+      add(List.of(0,0,0,0));
+      add(List.of(0,0,0,0));
+      add(List.of(1,0,0,0));
+    }};
+    assertEquals(expectedBlockState, connect4Game.getAllBlockStates());
+  }
+
+  @Test
+  public void testDropPieceOnOtherPiece(){
+    Player playerOne = new HumanPlayer("Player 1");
+    Player playerTwo = new HumanPlayer("Player 2");
+    connect4Game = new Connect4Game(gameType, playerOne, playerTwo, startPattern);
+    Coordinate pieceToPress = new Coordinate(0,0);
+    connect4Game.play(pieceToPress);
+    Coordinate pieceToPress2 = new Coordinate(0,0);
+    connect4Game.play(pieceToPress2);
+    List<List<Integer>> expectedBlockState = new ArrayList<>(){{
+      add(List.of(0,0,0,0));
+      add(List.of(2,0,0,0));
+      add(List.of(1,0,0,0));
+    }};
+    assertEquals(expectedBlockState, connect4Game.getAllBlockStates());
+  }
 }
