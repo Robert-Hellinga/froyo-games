790db612773b4b15691bc9572628d4c9a7a49878
robert.hellinga <robert.hellinga@duke.edu>
2020-11-19 03:50:18 -0500

    BUGFIX: Fixed States not updating properly for newly placed pieces in Othello, fixed win condition printing out the opposite player as the winner

--- src/ooga/model/checkerboard/block/OthelloBlock.java
+++ src/ooga/model/checkerboard/block/OthelloBlock.java
@@ -81,6 +81,7 @@ public class OthelloBlock extends Block {
   public void setPlayerID(int player){
     super.setPlayerID(player);
     state = playerID;
+    isPotentialMove = false;
   }
 
   @Override
