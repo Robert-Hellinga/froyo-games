b20c538734f1ce186dc99aeac14fc58ca0e669de
Lucas Carter <lbc21@duke.edu>
2020-11-18 20:24:21 -0500

    Revert "Fixed bug where you can move AI's pieces. Closes #33"
    This reverts commit ee6a0c13af6a4e56a7f3c62d811b192d47164b13

--- src/ooga/controller/IGameController.java
+++ src/ooga/controller/IGameController.java
@@ -4,6 +4,8 @@ import ooga.Coordinate;
 
 public interface IGameController {
 
+  void createGame();
+
   void clickPiece(Coordinate coordinate);
 
   void setClickingEnabled(boolean clickingEnabled);
